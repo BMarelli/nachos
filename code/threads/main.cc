e92f1786bb28320706273c9d455ca40064f1076d
BMarelli <marellibautista@gmail.com>
2021-04-06 17:09:36 -0300

    {Ejercicio 15, 16} terminados

--- code/threads/main.cc
+++ code/threads/main.cc
@@ -135,7 +135,7 @@ main(int argc, char **argv)
 #ifdef THREADS
         if (!strcmp(*argv, "-tt")) {         // Test the threading subsystem.
             ThreadTest();
-            interrupt->Halt();
+            // interrupt->Halt();
         }
 #endif
 #ifdef USER_PROGRAM
--- code/threads/thread_test_simple.cc
+++ code/threads/thread_test_simple.cc
@@ -11,6 +11,10 @@
 #include <stdio.h>
 #include <string.h>
 
+#ifdef SEMAPHORE_TEST
+#include "semaphore.hh"
+Semaphore *sem = new Semaphore("Semaphore", 3);
+#endif
 
 /// Loop 10 times, yielding the CPU to another ready thread each iteration.
 ///
@@ -22,6 +26,11 @@ SimpleThread(void *name_)
     // Reinterpret arg `name` as a string.
     char *name = (char *) name_;
 
+    #ifdef SEMAPHORE_TEST
+    DEBUG('s', "<thread %s> llamado a P()\n", name);
+    sem->P();
+    #endif
+
     // If the lines dealing with interrupts are commented, the code will
     // behave incorrectly, because printf execution may cause race
     // conditions.
@@ -30,6 +39,11 @@ SimpleThread(void *name_)
         currentThread->Yield();
     }
     printf("!!! Thread `%s` has finished\n", name);
+
+    #ifdef SEMAPHORE_TEST
+    DEBUG('s', "<thread %s> llamado a V()\n", name);
+    sem->V();
+    #endif
 }
 
 /// Set up a ping-pong between several threads.
