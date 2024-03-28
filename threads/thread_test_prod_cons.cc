afd178f5048fa8db846cca60de38f04f57fa6652
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-03-27 23:22:16 -0300

    feat(ej_2): add producer/consumer test case

--- threads/thread_test_prod_cons.cc
+++ threads/thread_test_prod_cons.cc
@@ -8,4 +8,130 @@
 
 #include <stdio.h>
 
-void ThreadTestProdCons() { printf("Test unimplemented!\n"); }
+#include <algorithm>
+#include <cstdlib>
+
+#include "condition.hh"
+#include "lock.hh"
+#include "system.hh"
+
+Lock *lock = new Lock("lock");
+Condition *empty = new Condition("empty", lock);
+Condition *full = new Condition("full", lock);
+
+static const int NUM_PRODUCERS = 20;
+static const int NUM_CONSUMERS = 2;
+
+static const long MAX_TO_PRODUCE = 100;
+static const long MAX_EXTRA_CAPACITY = 10;
+
+int buffer, capacity;
+
+long produced = 0;
+long consumed = 0;
+
+#define YIELD currentThread->Yield()
+
+void Producer(void *n) {
+    for (int i = 0; i < (long)n; i++) {
+        YIELD;
+
+        lock->Acquire();
+
+        YIELD;
+
+        while (buffer == capacity) {
+            empty->Wait();
+        }
+
+        YIELD;
+
+        buffer++;
+        produced++;
+        printf("%s produced %d.\n", currentThread->GetName(), buffer);
+
+        YIELD;
+
+        full->Signal();
+
+        YIELD;
+
+        lock->Release();
+
+        YIELD;
+    }
+}
+
+void Consumer(void *n) {
+    for (int i = 0; i < (long)n; i++) {
+        YIELD;
+
+        lock->Acquire();
+
+        YIELD;
+
+        while (buffer == 0) {
+            full->Wait();
+        }
+
+        YIELD;
+
+        buffer--;
+        consumed++;
+        printf("%s consumed %d.\n", currentThread->GetName(), buffer);
+
+        YIELD;
+
+        empty->Signal();
+
+        YIELD;
+
+        lock->Release();
+
+        YIELD;
+    }
+}
+
+void ThreadTestProdCons() {
+    long toProduce = 0;
+    long toConsume = 0;
+
+    // Launch a new thread for each producer.
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
+        printf("Launching producer %u.\n", i);
+
+        char *name = new char[16];
+        sprintf(name, "Producer %u", i);
+
+        Thread *t = new Thread(name);
+
+        long n = rand() % (MAX_TO_PRODUCE - toProduce + 1);
+
+        toProduce += n;
+        t->Fork(Producer, (void *)n);
+    }
+
+    // Launch a new thread for each consumer.
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        printf("Launching consumer %u.\n", i);
+
+        char *name = new char[16];
+        sprintf(name, "Consumer %u", i);
+
+        Thread *t = new Thread(name);
+
+        long n = rand() % (toProduce - toConsume + 1);
+
+        toConsume += n;
+        t->Fork(Consumer, (void *)n);
+    }
+
+    capacity = rand() % (MAX_EXTRA_CAPACITY - (toProduce - toConsume) + 1) + toProduce - toConsume;
+
+    // Wait until all producer and consumer threads finish their work.
+    while (produced < toProduce || consumed < toConsume) {
+        currentThread->Yield();
+    }
+
+    printf("All producers and consumers finished. Final buffer is %d (should be  %ld).\n", buffer, toProduce - toConsume);
+}
