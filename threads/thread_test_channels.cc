04fd479ee04137b11443d16a4323952ca9137238
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-05-11 20:18:22 -0300

    feat(channels): add more interactions on tests

--- threads/thread_test_channels.cc
+++ threads/thread_test_channels.cc
@@ -1,32 +1,70 @@
 #include "thread_test_channels.hh"
 
 #include <stdio.h>
-#include <string.h>
 
 #include "channels.hh"
+#include "system.hh"
 
-static Channels *channel = new Channels("TestChannel");
+Channels *channel = new Channels("TestChannel");
 
-void SenderThread(void *name_) {
-    char *name = (char *)name_;
-    int data = 42;
-    printf("*** Thread `%s`: Sending data %d\n", name, data);
-    channel->Send(data);
+#define YIELD currentThread->Yield()
+
+bool senderDone, receiverDone;
+
+void SenderThread(void *) {
+    printf("Sending 0\n");
+    channel->Send(0);
+    printf("Sent 0\n");
+
+    printf("Sending 1\n");
+    channel->Send(1);
+    printf("Sent 1\n");
+
+    printf("Sending 2\n");
+    channel->Send(2);
+    printf("Sent 2\n");
+
+    YIELD;
+
+    printf("Sending 3\n");
+    channel->Send(3);
+    printf("Sent 3\n");
+
+    senderDone = true;
 }
 
-void ReceiverThread(void *name_) {
-    char *name = (char *)name_;
+void ReceiverThread(void *) {
     int response;
+
+    printf("Receiving message\n");
     channel->Receive(&response);
-    printf("*** Thread `%s`: Received data %d\n", name, response);
+    printf("Received %d\n", response);
+
+    printf("Receiving message\n");
+    channel->Receive(&response);
+    printf("Received %d\n", response);
+
+    YIELD;
+
+    printf("Receiving message\n");
+    channel->Receive(&response);
+    printf("Received %d\n", response);
+
+    printf("Receiving message\n");
+    channel->Receive(&response);
+    printf("Received %d\n", response);
+
+    receiverDone = true;
 }
 
 void ThreadTestChannels() {
-    Thread *senderThread = new Thread("Sender");
-    Thread *receiverThread = new Thread("Receiver", true);
+    Thread *sender = new Thread("Sender");
+    Thread *receiver = new Thread("Receiver");
 
-    senderThread->Fork(SenderThread, (void *)"Sender");
-    receiverThread->Fork(ReceiverThread, (void *)"Receiver");
-    receiverThread->Join();
-    printf("!!! Threads have finished\n");
+    sender->Fork(SenderThread, nullptr);
+    receiver->Fork(ReceiverThread, nullptr);
+
+    while (!senderDone || !receiverDone) {
+        YIELD;
+    }
 }
