4a945ecc5a900764a3310d0e75c4ff3e3ec4e56d
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-05-25 12:40:43 -0300

    refactor(userland): use lib.c in cat.c

--- userland/cat.c
+++ userland/cat.c
@@ -1,31 +1,38 @@
 /// Outputs the contents of a file to the console.
 
+#include "lib.c"
 #include "syscall.h"
 
-#define ARGC_ERROR "Error: missing argument"
-#define OPEN_ERROR "Error: could not open file."
+#define ARGC_ERROR "Error: missing argument.\n"
+#define OPEN_ERROR "Error: could not open file: "
+
+#define BUFFER_SIZE 128
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        Write(ARGC_ERROR, sizeof(ARGC_ERROR) - 1, CONSOLE_OUTPUT);
+        puts(ARGC_ERROR);
         Exit(1);
     }
+
+    char buffer[BUFFER_SIZE];
 
     for (unsigned i = 1; i < argc; i++) {
         OpenFileId fid = Open(argv[i]);
         if (fid < 0) {
-            Write(OPEN_ERROR, sizeof(OPEN_ERROR) - 1, CONSOLE_OUTPUT);
+            puts(OPEN_ERROR);
+            puts(argv[i]);
             Exit(1);
         }
 
-        char buffer[128];
         int size;
-        while ((size = Read(buffer, sizeof(buffer), fid)) > 0) {
+        while ((size = Read(buffer, sizeof buffer, fid)) > 0) {
             Write(buffer, size, CONSOLE_OUTPUT);
         }
-        Write("\n", sizeof("\n"), CONSOLE_OUTPUT);
+
+        Write("\n", 1, CONSOLE_OUTPUT);
 
         Close(fid);
-        Exit(0);
     }
+
+    Exit(0);
 }
