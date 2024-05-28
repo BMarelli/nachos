32eaea9570d743188a8070abada0380ee3c528e7
Juan Cruz De La Torre <delatorrejuanchi@gmail.com>
2024-05-28 01:08:20 -0300

    feat(plancha_3-ej_1): implement methods to transfer memory from/to userspace/kernel

--- userprog/transfer.cc
+++ userprog/transfer.cc
@@ -7,7 +7,15 @@
 #include "threads/system.hh"
 
 void ReadBufferFromUser(int userAddress, char *outBuffer, unsigned byteCount) {
-    // TODO: implement.
+    ASSERT(userAddress != 0);
+    ASSERT(outBuffer != nullptr);
+    ASSERT(byteCount != 0);
+
+    for (unsigned i = 0; i < byteCount; i++) {
+        int temp;
+        ASSERT(machine->ReadMem(userAddress++, 1, &temp));
+        *outBuffer++ = (unsigned char)temp;
+    }
 }
 
 bool ReadStringFromUser(int userAddress, char *outString, unsigned maxByteCount) {
@@ -15,21 +23,34 @@ bool ReadStringFromUser(int userAddress, char *outString, unsigned maxByteCount)
     ASSERT(outString != nullptr);
     ASSERT(maxByteCount != 0);
 
-    unsigned count = 0;
-    do {
+    for (unsigned i = 0; i < maxByteCount; i++) {
         int temp;
-        count++;
         ASSERT(machine->ReadMem(userAddress++, 1, &temp));
-        *outString = (unsigned char)temp;
-    } while (*outString++ != '\0' && count < maxByteCount);
+        outString[i] = (unsigned char)temp;
 
-    return *(outString - 1) == '\0';
+        if (outString[i] == '\0') return true;
+    }
+
+    return false;
 }
 
 void WriteBufferToUser(const char *buffer, int userAddress, unsigned byteCount) {
-    // TODO: implement.
+    ASSERT(userAddress != 0);
+    ASSERT(buffer != nullptr);
+    ASSERT(byteCount != 0);
+
+    for (unsigned i = 0; i < byteCount; i++) {
+        ASSERT(machine->WriteMem(userAddress++, 1, (int)buffer[i]));
+    }
 }
 
 void WriteStringToUser(const char *string, int userAddress) {
-    // TODO: implement.
+    ASSERT(userAddress != 0);
+    ASSERT(string != nullptr);
+
+    for (unsigned i = 0; string[i] != '\0'; i++) {
+        ASSERT(machine->WriteMem(userAddress++, 1, (int)string[i]));
+    }
+
+    ASSERT(machine->WriteMem(userAddress, 1, (int)'\0'));
 }
